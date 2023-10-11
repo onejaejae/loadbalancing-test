ceaf5da6fb2329e4a6ac1e048e5577731d42616d
wonjjee <yoteamo7@naver.com>
2023-10-11 12:57:39 +0900

    feat: setup enviroment for loadbalancing testing

---
+++ app/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+const SERVER_ID = process.env.SERVER_ID;
+
+app.get("/", (req, res) => {
+  res.send({ server: SERVER_ID, port: PORT });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server ${SERVER_ID} is running at ${PORT}.`);
+});
